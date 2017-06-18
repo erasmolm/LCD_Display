0fa34e5e73e6b2b7c3fc117eb6e6df1e5419700a
erasmolm <erostheworst@gmail.com>
2017-06-18 17:35:05 +0200

    Update LCD_STM_Pin_Configurator.h

--- LCD_Display/LCD_STM_Pin_Configurator.h
+++ LCD_Display/LCD_STM_Pin_Configurator.h
@@ -27,6 +27,18 @@ typedef struct GPIOPort_TypeDef{
 	uint16_t pin_number;
 }GPIOPort_TypeDef;
 
+/*
+ * @brief Mapping dei pin del display sulle porte GPIO.
+ * 	Questa configurazione può essere modificata per
+ * 	rimappare i pin su porte GPIO differenti.
+ * */
+#define RS_PIN			(GPIOPort_TypeDef){GPIOC,GPIO_PIN_4}
+#define EN_PIN			(GPIOPort_TypeDef){GPIOB,GPIO_PIN_13}
+#define D4_PIN			(GPIOPort_TypeDef){GPIOB,GPIO_PIN_14}
+#define D5_PIN			(GPIOPort_TypeDef){GPIOB,GPIO_PIN_15}
+#define D6_PIN			(GPIOPort_TypeDef){GPIOB,GPIO_PIN_1}
+#define D7_PIN			(GPIOPort_TypeDef){GPIOB,GPIO_PIN_2}
+
 /**
   * @brief tipo di dato astratto PinConfig_TypeDef per gestire la configurazione
   * 	   dei pin del display.
